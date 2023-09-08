d1333bc497d70e938724a3d9ef03e4f8625aaede
Matthew Quigley <mquigs00@gmail.com>
2023-09-08 11:09:29 -0400

    Added rest of routes. Updated ReadMe.

--- LibrarySimWebsite/index.js
+++ LibrarySimWebsite/index.js
@@ -9,6 +9,13 @@ const subjects = require('./routes/subjects');
 const books = require('./routes/books');
 const users = require('./routes/users');
 const myaccount = require('./routes/myaccount');
+const searchItems = require('./routes/search-items');
+const aboutUs = require('./routes/about-us');
+const calendar = require('./routes/calendar');
+const contactUs = require('./routes/contact-us');
+const libraryCard = require('./routes/library-card');
+const policies = require('./routes/policies');
+const technologyInLibrary = require('./routes/technology-in-library');
 
 const app = express();
 const port = 3000;
@@ -25,7 +32,13 @@ app.use('/subjects', subjects);
 app.use('/books', books);
 app.use('/users', users);
 app.use('/myaccount', myaccount);
-
+app.use('/search-items', searchItems);
+app.use('/about-us', aboutUs);
+app.use('/calendar', calendar);
+app.use('/contact-us', contactUs);
+app.use('/library-card', libraryCard);
+app.use('/policies', policies);
+app.use('/technology-in-library', technologyInLibrary);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
