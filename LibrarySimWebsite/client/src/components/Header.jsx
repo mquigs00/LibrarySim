0e7f7f05aa29b132d59ac1f1a51ea694d8a77854
Matthew Quigley <mquigs00@gmail.com>
2023-09-14 13:54:57 -0400

    Moved webpage images from server to client

---
+++ LibrarySimWebsite/client/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <>
+        <p>125 Evergreen Lane Havenbrook, CT 12345 Tel: 123-456-7890</p>
+        <div>
+            <a href="">
+                <img src="../../public/assets/images/WP_Logo.png" alt="Library Logo" />
+            </a>
+        </div>
+    </>
+  )
+}
+
+export default Header
